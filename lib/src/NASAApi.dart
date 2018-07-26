3742d9ad10abc3a7b3871373acdb8b05c4b904f2
linusyoung <linusyoungrice@gmail.com>
2018-07-26 22:56:01 +1000

    add constructor to handle date query

--- lib/src/NASAApi.dart
+++ lib/src/NASAApi.dart
@@ -2,10 +2,18 @@ class NASAApi {
   static const API_KEY = "2bHuLGYETr9kzcrkqRWBqlJOP1c1AYfMXilVkeAl";
   static const BASE_URL ="https://api.nasa.gov/planetary/apod?";
 
+  final date;
+
   NASAApi();
-  
+
+  NASAApi.hasDate({this.date});
   String getUrl(){
-    final requestUrl = BASE_URL + 'api_key=' + API_KEY;
+    var requestUrl;
+    if (date == null){
+      requestUrl = BASE_URL + 'api_key=' + API_KEY;
+    } else {
+      requestUrl = BASE_URL + 'api_key=' + API_KEY + '&date=' +date;
+    } 
     return requestUrl;
   }
 }
