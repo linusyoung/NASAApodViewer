e999dced9d64f29a9dd636861e8ddb29a4166529
linusyoung <linusyoungrice@gmail.com>
2018-08-10 21:22:10 +1000

    remove api key before push to github

--- lib/src/NASAApi.dart
+++ lib/src/NASAApi.dart
@@ -1,7 +1,7 @@
 import 'package:apod_viewer/src/data_util.dart';
 
 class NASAApi {
-  static const String apiKey = "2bHuLGYETr9kzcrkqRWBqlJOP1c1AYfMXilVkeAl";
+  static const String apiKey = "<YOUR KEY HERE>";
   static const String baseUrl = "https://api.nasa.gov/planetary/apod";
   static DateTime minDate = DateTime(1995, 6, 16);
   // Nasa Apod server on UTC-5
